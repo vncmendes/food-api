ba66a95389102e34cbdfd1b07bdd30fc823390f8
vncmendes <viniciussmendes@gmail.com>
2025-04-28 08:24:03 -0300

    adding `jwt` and `cookie` making `authorization`

---
+++ src/http/auth.ts
@@ -0,0 +1,13 @@
+import { env } from "@/env";
+import jwt from "@elysiajs/jwt";
+import { Elysia,  t } from "elysia";
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String())
+      })
+  }));
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,93 @@
-import { restaurants, users } from "@/database/schema";
+import { restaurants, users, authLinks } from "@/database/schema";
 import { database } from "@/database/seedsConnection";
+import { env } from "@/env";
+import { createId } from "@paralleldrive/cuid2";
+import dayjs from "dayjs";
+import { eq } from "drizzle-orm";
 import { Elysia, t } from "elysia";
+import { auth } from "./auth";
 
 const app = new Elysia()
-	.post("/restaurants", async ({ body, set}) => {
+  .use(auth)
+
+  .get("/", () => { 
+    return new Response("Hello World !");
+  })
+
+  .post("/users", () => {})
+
+  .post("/authenticate", async ({ body }) => {
+    const { email } = body;
+    const [userExists] = await database.select().from(users).where(eq(users.email, email));
+    
+    if (!userExists) {
+      throw new Error("User not found !");
+    }
+    const authLinkCode = createId();
+    
+    await database.insert(authLinks).values({
+      code: authLinkCode,
+      userId: userExists.id
+    });
+    // enviar um email
+    const authLink = new URL("/auth-links/authenticate", env.API_BASE_URL);
+    // generate link
+    authLink.searchParams.set("code", authLinkCode);
+    // redirect
+    authLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL);
+    console.log(authLink.toString());
+  }, {
+    body: t.Object({
+      email: t.String({ format: "email" }),
+    })
+  })
+
+  .get("/auth-links/authenticate", async ({ query, jwt, cookie: { auth }}) => {
+    const { code, redirect } = query;
+
+    const [authLinkFromCode] = await database.select().from(authLinks).where(eq(authLinks.code, code));
+    // const authLinkFromCode = await database.query.authLinks.findFirst({where(fields, { eq }) { return eq(fields.code, code); }, });
+
+    if (!authLinkFromCode) {
+      throw new Error("Unauthorized !");
+    }
+
+    const daysSinceAuthLinkWasCreated = dayjs().diff(
+      authLinkFromCode.createdAt, 
+      "days"
+    );
+
+    if (daysSinceAuthLinkWasCreated > 7) {
+      throw new Error("Auth Token Expired !, Try again");
+    }
+
+    const [managerRestaurant] = await database.select()
+      .from(restaurants)
+      .where(eq(restaurants.managerId, authLinkFromCode.userId)
+    );
+
+    const token = await jwt.sign({
+      sub: authLinkFromCode.userId,
+      restaurantId: managerRestaurant?.id
+    });
+
+    auth.value = token;
+    auth.httpOnly = true;
+    auth.maxAge = 60 * 60 * 24 * 7; // 7 dias
+    auth.path = "/";
+
+    await database.delete(authLinks).where(eq(authLinks.code, code));
+
+    return redirect;
+    // set.redirect = redirect; deprecated !!!
+  }, {
+    query: t.Object({
+      code: t.String(),
+      redirect: t.String()
+    })
+  })
+
+	.post("/restaurants", async ({ body, set }) => {
     const { restaurantName, managerName, email, phone } = body;
     
     const [manager] = await database.insert(users).values({
